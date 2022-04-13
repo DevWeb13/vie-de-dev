e914ef1de18d1dda0f419a2cdd56a8a005a4cfdd
DevWeb13 <giuliano.loic@sfr.fr>
2022-04-13 10:00:33 +0200

    feat: Create ConnectModal.jsx ; Login.jsx ; SignUp.jsx ; .prettierrc

---
+++ src/components/ConnectModal.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ConnectModal = () => {
+  return (
+    <div className='connect-modal'>
+      <div className="header-btn">
+        <button>S'inscrire</button>
+        <button>Se connecter</button>
+</div>
+    </div>
+  );
+};
+
+export default ConnectModal;

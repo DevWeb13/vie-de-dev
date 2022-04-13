f3cbe27e21e34e86e666230d6c782aad0ce78102
DevWeb13 <giuliano.loic@sfr.fr>
2022-04-13 10:33:32 +0200

    feat: SignUp.jsx
    Create Formulaire

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 const SignUp = () => {
   return (
-    <div>
-      <h1>Signup</h1>
+    <div className="signup-container">
+      <div className="signup">
+        <h3>S'inscrire</h3>
+        <form>
+          <input type="text" placeholder="Pseudo" required />
+          <input type="email" placeholder="Email" required />
+          <input type="password" placeholder="Mot de passe" required />
+          <input type="submit" value="Valider inscription" />
+        </form>
+      </div>
     </div>
   );
 };

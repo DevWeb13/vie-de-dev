fcb6226428ec53edf30933fcae570eea8b0ab83b
DevWeb13 <giuliano.loic@sfr.fr>
2022-04-13 10:25:32 +0200

    feat: ConnectModal
    add style and onClick at button

--- src/components/ConnectModal.jsx
+++ src/components/ConnectModal.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ConnectModal = () => {
+  const [signUp, setSignUp] = useState(true);
   return (
-    <div className='connect-modal'>
+    <div className="connect-modal">
       <div className="header-btn">
-        <button>S'inscrire</button>
-        <button>Se connecter</button>
-</div>
+        <button
+          style={{ background: signUp ? 'rgb(28,28,28)' : 'rgb(83,83,83)' }}
+          onClick={() => setSignUp(true)}
+        >
+          S'inscrire
+        </button>
+
+        <button
+          style={{ background: signUp ? 'rgb(83,83,83)' : 'rgb(28,28,28)' }}
+          onClick={() => setSignUp(false)}
+        >
+          Se connecter
+        </button>
+      </div>
     </div>
   );
 };

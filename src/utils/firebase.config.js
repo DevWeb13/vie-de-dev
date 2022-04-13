d239e214eec7393efaa7a825db865afc2fecb8d4
DevWeb13 <giuliano.loic@sfr.fr>
2022-04-13 10:55:15 +0200

    feat: SignUp.jsx
    Create function handleRegister(e) for create new user

--- src/utils/firebase.config.js
+++ src/utils/firebase.config.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import firebase from "firebase/compat/app";
+import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from 'firebase/analytics';
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyDhIWeoFyXTnOg1olGRquFNIGLbs8YAYZ4",
-  authDomain: "vie-de-dev-api.firebaseapp.com",
-  projectId: "vie-de-dev-api",
-  storageBucket: "vie-de-dev-api.appspot.com",
-  messagingSenderId: "27768840204",
-  appId: "1:27768840204:web:7c71735570f9329834ce29",
-  measurementId: "G-86W4H44XR8"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: 'vie-de-dev-api.firebaseapp.com',
+  projectId: 'vie-de-dev-api',
+  storageBucket: 'vie-de-dev-api.appspot.com',
+  messagingSenderId: '27768840204',
+  appId: '1:27768840204:web:7c71735570f9329834ce29',
+  measurementId: 'G-86W4H44XR8',
 });
 
 const analytics = getAnalytics(app);
